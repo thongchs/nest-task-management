de0b41af1e0d37657b710747875714c154a8155a
thongchs <thongchs@thongchss-MacBook-Pro.local>
2023-12-18 18:57:48 +0700

    add CRUD

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -10,6 +10,20 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  deleteTaskById(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
     const task: Task = {
